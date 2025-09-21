c45eb56dff6b67213712f6f6c3ff8ad1f63ba100
François Roland <francois.roland@heh.be>
2025-09-21 17:07:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TD1
+
+Crée une fonction en TypeScript nommée `messageAccueil` qui :
+
+1. Prend en paramètre une chaîne de caractères représentant le nom d’une personne.
+2. Retourne un message d’accueil personnalisé au format : `Bonjour, <nom>! Bienvenue.`
---
+++ src/test/index.test.ts
@@ -0,0 +1,10 @@
+import {messageAccueil} from '../index';
+
+describe('Message d’accueil personnalisé ', () => {
+    test('Doit retouner le message : Bonjour, Alice! Bienvenue.', () => {
+        expect(messageAccueil("Alice")).toEqual('Bonjour, Alice! Bienvenue.');
+    });
+    test('Doit retouner le message : Bonjour, Bob! Bienvenue.', () => {
+        expect(messageAccueil("Bob")).toEqual('Bonjour, Bob! Bienvenue.');
+    });
+});
